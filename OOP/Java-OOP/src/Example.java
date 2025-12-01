fa1f67e4d8f35437fc55da4fe19722a81b7aeb30
unknown <harkdevx@gmail.com>
2025-12-01 16:46:39 +0800

    feat: notes(super-keyword, this-keyword, static-keyword)

---
+++ OOP/Java-OOP/src/Example.java
@@ -0,0 +1,29 @@
+
+//class Example {
+//
+//    String instanceVar = "instance";
+//
+//    static String staticVar = "static";
+//
+//    static void instanceMethod() {
+//        // Cannot access instance variable;
+//        System.out.println(instanceVar);
+//
+//        // same with this
+//        anotherInstanceMethod();
+//
+//        // can can access static variable
+//        System.out.print(staticVar);
+//
+//        // can access static method
+//        staticMethod();
+//    }
+//
+//    public void anotherInstanceMethod() {
+//        System.out.println("another instance method");
+//    }
+//
+//    static void staticMethod() {
+//        System.out.println("static method");
+//    }
+//}
